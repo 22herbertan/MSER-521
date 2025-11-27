4028c4c1bab316d647bdef8ea4351e2e9c7f4b1e
Angelina <herbert.angelina04@gmail.com>
2025-11-27 00:33:55 -0500

    style changes

--- lab05/05-carousel/index.js
+++ lab05/05-carousel/index.js
@@ -12,19 +12,19 @@ const totalSlides = items.length;
 
 // move carousel to the next image
 function moveForward() {
-  if (currentPosition >= totalSlides - 2) { 
+  if (currentPosition >= totalSlides - 2) {  // if position is greater than or equal to the number of images, reset it to the start
     currentPosition = 0;
   } else {
-    currentPosition++;
+    currentPosition++; // otherwise, advance to next image
   }
   updateCarouselPosition(); // trigger transformation
 }
 // move carousel to the previous image
 function moveBackward() {
-  if (currentPosition <= 0) {
+  if (currentPosition <= 0) { // if position is less than or equal to the number of images, reset it to the start
     currentPosition = totalSlides - 2;
   } else {
-    currentPosition--;
+    currentPosition--; // otherwise, advance to previous image
   }
   updateCarouselPosition(); // trigger transformation
 }
@@ -32,7 +32,7 @@ function moveBackward() {
 // calculate and apply the transform
 function updateCarouselPosition() {
   const items = document.querySelectorAll('.carousel-item');
-  const offset = (slideWidth + gap) * currentPosition;
+  const offset = (slideWidth + gap) * currentPosition; // transformation equation
 
   for (const item of items) {
     item.style.transform = `translateX(-${offset}px)`; // move the image horizontally by offset
