4a424eb6b1ffacc1b1f75392d7e362b270981593
Angelina <herbert.angelina04@gmail.com>
2025-12-02 14:24:09 -0500

    progress on leaflet map

--- final/VWINmap/main.js
+++ final/VWINmap/main.js
@@ -14,66 +14,66 @@ async function getChartData() {
   const response = await fetch(
     `https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbyKNAa0glXuL_enuIUsGIzQ_JzcxsCIvzGJsaE0z-MDJO2f-0PRYTmiOnGX47b448vV/exec?sheet=${sheetName}`
   );
-  const landmarks = await response.json();
-  console.log('serverData: ', landmarks);
-  return landmarks;
+  const sites = await response.json();
+  console.log('serverData: ', sites);
+  return sites;
 }
 
 const marker = L.marker([35.616988, -82.565401], {
     icon: getIcon('teal', '📚'),  // here is the place where you override the default marker with your own custom style
 }).addTo(map);
 
-function getIcon(color, icon) {
-    return L.divIcon({
-        html: `
-            <div style="background-color: ${color}" class="map-marker">
-                ${icon}
-            </div>
-        `,
-        iconSize: [50, 50],
-        iconAnchor: [25, 25],
-    });
-}
+// function getIcon(color, icon) {
+//     return L.divIcon({
+//         html: `
+//             <div style="background-color: ${color}" class="map-marker">
+//                 ${icon}
+//             </div>
+//         `,
+//         iconSize: [50, 50],
+//         iconAnchor: [25, 25],
+//     });
+// }
 
 async function generateMarkers() {
-  const landmarks = await getChartData();
+  const sites = await getChartData();
 
-  // Add markers for each landmark
-  for (const landmark of landmarks) {
+  // Add markers for each site
+  for (const site of sites) {
     // Create marker:
-    const marker = L.marker([landmark.lat, landmark.lng], {
-    icon: getIcon(landmark.color, landmark.icon),  // here is the place where you override the default marker with your own custom style
+    const marker = L.marker([site.LAT, site.LON], {
+    icon: getIcon(site.color, site.icon),  // here is the place where you override the default marker with your own custom style
 }).addTo(map);
 
-    marker.bindPopup(getPopupTemplate(landmark));
+    marker.bindPopup(getPopupTemplate(site));
     marker.on('click', function (e) {
-      detailsElement.innerHTML = getPanelTemplate(landmark);
+      detailsElement.innerHTML = getPanelTemplate(site);
     });
   }
 
   detailsElement.innerHTML = '';
 }
 
-function getPopupTemplate(landmark) {
+function getPopupTemplate(site) {
   return `
         <div>
-            ${landmark.icon} ${landmark.name}<br>
+            ${site.icon} ${site.Name}<br>
         </div>
     `;
 }
 
-function getPanelTemplate(landmark) {
+function getPanelTemplate(site) {
   return `
       <div>
-          <div class="emoji">${landmark.icon}</div>
-          <h3>${landmark.name}</h3>
+          <div class="emoji">${site.icon}</div>
+          <h3>${site.Name}</h3>
           <p class="tag">
-            ${landmark.category.replace('_', ' ')}
+            ${site.County_Code.replace('_', ' ')}
           </p>
-          <p>${landmark.description}</p>
-          <img src=${landmark.online_image}/>
+          <p>${site.Description}</p>
+          <img src=${site.Image}/>
           <p>
-            <strong>Coordinates:</strong> ${landmark.lat.toFixed(4)}, ${landmark.lng.toFixed(4)}
+            <strong>Coordinates:</strong> ${site.LAT.toFixed(4)}, ${site.LON.toFixed(4)}
           </p>
       </div>
       `;
