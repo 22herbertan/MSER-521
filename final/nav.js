4e4d3db4b21fae275f6539ca857269301e1c40fd
Angelina <herbert.angelina04@gmail.com>
2025-12-07 17:20:57 -0500

    added accordion panel

--- final/nav.js
+++ final/nav.js
@@ -23,3 +23,23 @@ document.body.addEventListener('click', function (ev) {
 
 // Close menu when clicking outside of nav-links:
 document.querySelector('.navigation').addEventListener('click', toggleMenu);
+
+// Accordion panel toggle
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+} 
