07a17c919c62a0c64868f36bce433837e70b4593
Angelina <herbert.angelina04@gmail.com>
2025-12-08 20:09:24 -0500

    update

---
+++ final/VWINsites/nav.js
@@ -0,0 +1,42 @@
+// Hamburger menu toggle
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+
+function toggleMenu(ev) {
+  // Accessibility:Toggle aria-expanded
+  const isExpanded = hamburger.getAttribute('aria-expanded') === 'true';
+  hamburger.setAttribute('aria-expanded', !isExpanded);
+  nav.classList.toggle('active');
+
+  // Prevent event from bubbling up to the body
+  ev.stopPropagation();
+}
+
+// Close menu when clicking outside of nav-links:
+document.body.addEventListener('click', function (ev) {
+  if (ev.target.closest('.nav-links')) {
+    return;
+  }
+  hamburger.setAttribute('aria-expanded', 'false');
+  nav.classList.remove('active');
+});
+
+// Close menu when clicking outside of nav-links:
+document.querySelector('.navigation').addEventListener('click', toggleMenu);
+
+// Accordion panel toggle
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    // Toggle "active" class on the clicked accordion button
+    this.classList.toggle("active");
+
+    // Get the associated panel (next sibling)
+    var panel = this.nextElementSibling;
+
+    // Toggle the "active" class on the panel to show/hide it via CSS
+    panel.classList.toggle("active");
+  });
+}
